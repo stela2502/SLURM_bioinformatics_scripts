779e3587291be2b2f702d84c48c1e5da064994e1
Stefan Lang <Stefan.Lang@med.lu.se>
2016-05-31 08:38:13 +0200

    Initial untested version - no sxcripts no functionallity.

---
+++ lib/SLURM_bioinformatic_scripts.pm
@@ -0,0 +1,15 @@
+package SLURM_bioinformatic_scripts;
+
+our $VERSION = '1.01';
+
+
+=head1 AUTHOR
+
+Stefan Lang
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
