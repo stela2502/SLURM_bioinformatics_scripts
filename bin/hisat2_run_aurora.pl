8bd7c490a1d27c80772b125c0f868d61de107f5e
Stefan <med-sal@pinkman.bmc.lu.se>
2016-06-13 14:40:40 +0200

    Added hisat2 support.

--- bin/hisat2_run_aurora.pl
+++ bin/hisat2_run_aurora.pl
@@ -148,6 +148,24 @@ foreach (qw(n N t mem)) {
 	delete( $options->{$_} );
 }
 
+$fm = root->filemap( $files[0] );
+open( SC, ">$fm->{'path'}/InitializeSLURMenv.sh" )
+  or die "I could not create the SLURM init script\n$!\n";
+
+foreach (
+	'icc/2016.1.150-GCC-4.9.3-2.25 impi/5.1.2.150 HISAT2/2.0.4',
+	'GCC/4.9.3-2.25 OpenMPI/1.10.2',
+	'icc/2015.3.187-GNU-4.9.3-2.25  impi/5.0.3.048',
+	'SAMtools/0.1.20 BEDTools/2.25.0',
+  )
+{
+	print SC "module load $_\n";
+}
+close(SC);
+unless ($debug) {
+	system("bash $fm->{'path'}/InitializeSLURMenv.sh");
+}
+
 my $BAM = stefans_libs::scripts::BAM->new($options);
 
 @files = map { $_ =~ s/^\.\///; $_ } @files;
--- t/data/test_empty.fastq.sh
+++ t/data/test_empty.fastq.sh
@@ -5,11 +5,11 @@
 #SBATCH -J test_empty.fastq
 #SBATCH -o test_empty.fastq%j.out
 #SBATCH -e test_empty.fastq%j.err
-hisat -x /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/hg38/hg38 -U /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/test_empty.fastq.gz --threads 2 > /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sam
-samtools view -Sb  /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sam | samtools sort -@ 1 - /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sorted
-if  [ -f /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sorted.bam ]&&[ -s /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sorted.bam ]; then
-rm -f /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sam
+hisat2 -x /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/hg38/hg38 -U /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/test_empty.fastq.gz --threads 2 > /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sam
+samtools view -Sb  /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sam | samtools sort -@ 1 - /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sorted
+if  [ -f /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sorted.bam ]&&[ -s /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sorted.bam ]; then
+rm -f /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sam
 fi
-bedtools genomecov -bg -split -ibam /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.sorted.bam -g /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/fake_hg38.chrom.sizes.txt | sort -k1,1 -k2,2n > /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.bedGraph
-bedGraphToBigWig /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.bedGraph /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/fake_hg38.chrom.sizes.txt /home/slang/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT_OUT/test_empty.fastq_hisat.bw
+bedtools genomecov -bg -split -ibam /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.sorted.bam -g /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/fake_hg38.chrom.sizes.txt | sort -k1,1 -k2,2n > /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.bedGraph
+bedGraphToBigWig /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.bedGraph /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/fake_hg38.chrom.sizes.txt /home/med-sal/git_Projects/SLURM_bioinformatics_scripts/t/data/HISAT2_OUT/test_empty.fastq_hisat.bw
 
