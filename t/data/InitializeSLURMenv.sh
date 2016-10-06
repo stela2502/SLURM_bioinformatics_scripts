fd379d6dd4741bae30149f461237808e9c83c19d
Stefan Lang <Stefan.Lang@med.lu.se>
2016-10-06 14:20:00 +0200

    update git?

--- t/data/InitializeSLURMenv.sh
+++
@@ -1,4 +0,0 @@
-module load icc/2016.1.150-GCC-4.9.3-2.25 impi/5.1.2.150 HISAT2/2.0.4
-module load GCC/4.9.3-2.25 OpenMPI/1.10.2
-module load icc/2015.3.187-GNU-4.9.3-2.25  impi/5.0.3.048
-module load SAMtools/0.1.20 BEDTools/2.25.0
