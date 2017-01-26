664dcaaac9863d3df9b3059485f3ae42da4a2791
stefanl <stefanl@aurora-scc.int>
2017-01-26 09:03:34 +0100

    The bigwig tracks are still broken.

--- bin/hisat2_run_aurora.pl
+++ bin/hisat2_run_aurora.pl
@@ -38,6 +38,9 @@
        -coverage     :the chromome length file
        -paired       :analyse paried fasta files 
                       every first file == read 1 every second == read2
+                      
+       -dropDuplicates :use picard to drop duplicates
+       
        -bigwigTracks :the bigwig tracks file you can upload to the USCS genome browser
 
 
@@ -70,65 +73,66 @@ my $dir = getcwd . "/";
 
 my $VERSION = 'v1.0';
 
-
-my ( $help, $debug, $database, @files, $options, @options, $genome, $coverage, $paired, $bigwigTracks, $sra, $outpath, $max_jobs);
-
-Getopt::Long::GetOptions(
-       "-files=s{,}"    => \@files,
-       "-options=s{,}"    => \@options,
-	 "-genome=s"    => \$genome,
-	 "-coverage=s"    => \$coverage,
-	 "-outpath=s" => \$outpath,
-	 "-paired"    => \$paired,
-	 "-bigwigTracks=s"    => \$bigwigTracks,
-	 "-sra"    => \$sra,
-	 "-max_jobs=s" => \$max_jobs,
-
-	 "-help"             => \$help,
-	 "-debug"            => \$debug
+my (
+	$help,         $debug,          $database, @files,    $options,
+	@options,      $dropDuplicates, $genome,   $coverage, $paired,
+	$bigwigTracks, $sra,            $outpath,  $max_jobs
 );
 
-my $warn = '';
+Getopt::Long::GetOptions(
+	"-files=s{,}"     => \@files,
+	"-options=s{,}"   => \@options,
+	"-genome=s"       => \$genome,
+	"-coverage=s"     => \$coverage,
+	"-outpath=s"      => \$outpath,
+	"-paired"         => \$paired,
+	"-bigwigTracks=s" => \$bigwigTracks,
+	"-sra"            => \$sra,
+	"-max_jobs=s"     => \$max_jobs,
+	"-dropDuplicates" => \$dropDuplicates,
+
+	"-help"  => \$help,
+	"-debug" => \$debug
+);
+
+my $warn  = '';
 my $error = '';
 
-unless ( defined $files[0]) {
+unless ( defined $files[0] ) {
 	$error .= "the cmd line switch -files is undefined!\n";
 }
-unless ( defined $options[0]) {
+unless ( defined $options[0] ) {
 	$error .= "the cmd line switch -options is undefined!\n";
 }
-unless ( defined $genome) {
+unless ( defined $genome ) {
 	$error .= "the cmd line switch -genome is undefined!\n";
 }
-unless ( -f $coverage) {
+unless ( -f $coverage ) {
 	$error .= "the cmd line switch -coverage is undefined ($coverage)!\n";
 }
-unless ( defined $bigwigTracks) {
+unless ( defined $bigwigTracks ) {
 	$error .= "the cmd line switch -bigwigTracks is undefined!\n";
 }
-unless ( $max_jobs ) {
+unless ($max_jobs) {
 	$max_jobs = 40;
 }
 
-
-
-if ( $help ){
-	print helpString( ) ;
+if ($help) {
+	print helpString();
 	exit;
 }
 
-if ( $error =~ m/\w/ ){
-	helpString($error ) ;
+if ( $error =~ m/\w/ ) {
+	helpString($error);
 	exit;
 }
 
 sub helpString {
 	my $errorMessage = shift;
-	$errorMessage = ' ' unless ( defined $errorMessage); 
+	$errorMessage = ' ' unless ( defined $errorMessage );
 	print "$errorMessage.\n";
-	pod2usage(q(-verbose) => 1);
+	pod2usage( q(-verbose) => 1 );
 }
-
 
 for ( my $i = 0 ; $i < @options ; $i += 2 ) {
 	$options[ $i + 1 ] =~ s/\n/ /g;
@@ -136,27 +140,30 @@ for ( my $i = 0 ; $i < @options ; $i += 2 ) {
 }
 ### initialize default options:
 
-$options->{'n'} ||= 10;
-$options->{'N'} ||= 1;
-$options->{'t'} ||= '02:00:00';
-$options->{'proc'} ||= $options->{'n'}*$options->{'N'};
-$options->{'p'} ||= $options->{'proc'};
+$options->{'n'}    ||= 10;
+$options->{'N'}    ||= 1;
+$options->{'t'}    ||= '02:00:00';
+$options->{'proc'} ||= $options->{'n'} * $options->{'N'};
+$options->{'p'}    ||= $options->{'proc'};
 
 ###
- 
-my ( $task_description);
 
-$task_description .= 'perl '.$plugin_path .'/hisat2_run_aurora.pl';
-$task_description .= ' -files "'.join( '" "', @files ).'"' if ( defined $files[0]);
-$task_description .= ' -options "'.join( '" "', @options ).'"' if ( defined $options[0]);
-$task_description .= " -genome '$genome'" if (defined $genome);
-$task_description .= " -coverage '$coverage'" if (defined $coverage);
-$task_description .= " -paired" if ($paired);
-$task_description .= " -bigwigTracks '$bigwigTracks'" if (defined $bigwigTracks);
-$task_description .= " -sra" if ($sra);
+my ($task_description);
+
+$task_description .= 'perl ' . $plugin_path . '/hisat2_run_aurora.pl';
+$task_description .= ' -files "' . join( '" "', @files ) . '"'
+  if ( defined $files[0] );
+$task_description .= ' -options "' . join( '" "', @options ) . '"'
+  if ( defined $options[0] );
+$task_description .= " -genome '$genome'"     if ( defined $genome );
+$task_description .= " -coverage '$coverage'" if ( defined $coverage );
+$task_description .= " -paired"               if ($paired);
+$task_description .= " -bigwigTracks '$bigwigTracks'"
+  if ( defined $bigwigTracks );
+$task_description .= " -sra"            if ($sra);
+$task_description .= " -dropDuplicates" if ($dropDuplicates);
 $task_description .= " -max_jobs $max_jobs";
-$task_description .= " -debug" if ($debug);
-
+$task_description .= " -debug"          if ($debug);
 
 ## Do whatever you want!
 
@@ -175,7 +182,7 @@ open( SC, ">$fm->{'path'}/InitializeSLURMenv.sh" )
   or die "I could not create the SLURM init script\n$!\n";
 
 foreach (
-	'icc/2016.1.150-GCC-4.9.3-2.25 impi/5.1.2.150 HISAT2/2.0.4',
+	'icc/2016.1.150-GCC-4.9.3-2.25 impi/5.1.2.150 HISAT2/2.0.4 BEDTools/2.25.0',
 	stefans_libs::scripts::BAM->SLURUM_load(),
   )
 {
@@ -191,105 +198,126 @@ my $BAM = stefans_libs::scripts::BAM->new($options);
 @files = map { $_ =~ s/^\.\///; $_ } @files;
 my $submitted = 0;
 
-while ( scalar(@files) ){
-	$fm  = root->filemap( $files[0] ); ## the files will be depleted by the create_call function!
-	$cmd = &chk_cmd(&create_call());
-	$cmd .= &chk_cmd($BAM->convert_sam_2_sorted_bam($this_outfile));
-	$cmd .= &chk_cmd($BAM->convert_sorted_bam_2_bedGraph($this_outfile,$coverage));
-#	$cmd .= &chk_cmd($BAM->convert_bedGraph_2_bigwig($this_outfile,$coverage)); # brken in the scripts
-	$tmp = $SLURM->run( $cmd, $fm, $this_outfile);
-	$submitted ++ if ( $tmp == 1 );
+while ( scalar(@files) ) {
+	$fm = root->filemap( $files[0] )
+	  ;    ## the files will be depleted by the create_call function!
+	$cmd = &chk_cmd( &create_call() );
+	$cmd .= &chk_cmd( $BAM->convert_sam_2_sorted_bam($this_outfile) );
+	$cmd .= &chk_cmd(create_picard_call($this_outfile));
+	$cmd .=
+	  &chk_cmd(
+		$BAM->convert_sorted_bam_2_bedGraph( $this_outfile, $coverage ) );
+
+	#$cmd .= &chk_cmd($BAM->convert_bedGraph_2_bigwig($this_outfile,$coverage));
+	$tmp = $SLURM->run( $cmd, $fm, $this_outfile );
+	$submitted++ if ( $tmp == 1 );
 	if ( $submitted >= $max_jobs ) {
 		$submitted -= 50;
-		&wait_for_last_finished( $this_outfile );
+		&wait_for_last_finished($this_outfile);
 	}
-}	
+}
 
-if ( @{$BAM->{'big_wig_urls'}} > 0 ) {
+if ( @{ $BAM->{'big_wig_urls'} } > 0 ) {
 	open( OUT, ">$bigwigTracks" )
 	  or die "I could not create the bigwig outfile '$bigwigTracks'\n$!\n";
-	print OUT join( "\n", @{$BAM->{'big_wig_urls'}} );
+	print OUT join( "\n", @{ $BAM->{'big_wig_urls'} } );
 	close(OUT);
-	print join( "\n", @{$BAM->{'big_wig_urls'}} ) . "\n\n";
+	print join( "\n", @{ $BAM->{'big_wig_urls'} } ) . "\n\n";
 	open( LOG, ">$bigwigTracks.log" )
 	  or die "I could not open the log file '$bigwigTracks.log'\n$!\n";
 	print LOG $task_description . "\n";
 	close(LOG);
 }
 
-
 print "Done\n";
 
-
 sub wait_for_last_finished {
-	my ( $fname ) = @_;
+	my ($fname) = @_;
 	my $wait;
-	while( $wait = &in_pipeline() > 4 ) {
+	while ( $wait = &in_pipeline() > 4 ) {
 		warn "waiting for $wait processes\n";
-		sleep( 50);
+		sleep(50);
 	}
 }
 
 sub in_pipeline {
-	open ( IN ,"whoami |" ) or die $!;
+	open( IN, "whoami |" ) or die $!;
 	my @IN = <IN>;
-	close ( IN );
+	close(IN);
 	my $name = $IN[0];
 	chomp($name);
-	open(IN, "squeue -u $name |" ) or die $!;
+	open( IN, "squeue -u $name |" ) or die $!;
 	@IN = <IN>;
-	close ( IN );
+	close(IN);
 	## pending
-	return scalar(grep ( /\sPD\s/, @IN));
+	return scalar( grep ( /\sPD\s/, @IN ) );
 	## all
-	return scalar(@IN)-1;
+	return scalar(@IN) - 1;
 }
 
+sub create_picard_call {
+	my ($file) = @_;
+	my $fm     = root->filemap($file);
+	my $p      = $outpath;
+	$p ||= "$fm->{'path'}/";
+	my $s =
+	    "picard MarkDuplicates INPUT="
+	  . $fm->{'total'}
+	  . " OUTPUT="
+	  . "$p$fm->{'filename_core'}_picard_deduplicated.bam"
+	  . " REMOVE_DUPLICATES=TRUE M="
+	  . "$p$fm->{'filename_core'}_picard_metrix.txt";
+	return $s, "$p$fm->{'filename_core'}_picard_deduplicated.bam",
+	"$p$fm->{'filename_core'}_picard_metrix.txt";
+}
 
 sub create_call {
-	return &create_paired_call() if ( $paired );
-	return &create_sra_call() if ( $sra );
-    my $file = shift(@files);
-    my $fm   = root->filemap($file);
-	my $p = $outpath;
+	return &create_paired_call() if ($paired);
+	return &create_sra_call()    if ($sra);
+	my $file = shift(@files);
+	my $fm   = root->filemap($file);
+	my $p    = $outpath;
 	$p ||= "$fm->{'path'}/HISAT2_OUT/";
-    mkdir ( "$p/" ) unless ( -d "$p/");
-    $fm->{'path'} = "" if ( $fm->{'path'} eq "./" );
-    unless ( $fm->{'path'} =~ m/^\// ) { $fm->{'path'} = $dir . $fm->{'path'}; }
-    my $s =
+	mkdir("$p/") unless ( -d "$p/" );
+	$fm->{'path'} = "" if ( $fm->{'path'} eq "./" );
+	unless ( $fm->{'path'} =~ m/^\// ) { $fm->{'path'} = $dir . $fm->{'path'}; }
+	my $s =
 "hisat2 -x $genome -U $fm->{'total'} --threads $options->{proc} --add-chrname > $p/$fm->{'filename_core'}_hisat.sam\n";
-    return $s, "$p/$fm->{'filename_core'}_hisat.sam", "$p/$fm->{'filename_core'}_hisat.sorted.bam";
+	return $s, "$p/$fm->{'filename_core'}_hisat.sam",
+	  "$p/$fm->{'filename_core'}_hisat.sorted.bam";
 }
 
 sub create_sra_call {
-    my $file = shift( @files);
-    my $fm   = root->filemap($file);
-	my $p = $outpath;
+	my $file = shift(@files);
+	my $fm   = root->filemap($file);
+	my $p    = $outpath;
 	$p ||= "$fm->{'path'}/HISAT2_OUT/";
-    mkdir ( "$p/" ) unless ( -d "$p/");
-    $fm->{'path'} = "" if ( $fm->{'path'} eq "./" );
-    unless ( $fm->{'path'} =~ m/^\// ) { $fm->{'path'} = $dir . $fm->{'path'}; }
-    my $s =
+	mkdir("$p/") unless ( -d "$p/" );
+	$fm->{'path'} = "" if ( $fm->{'path'} eq "./" );
+	unless ( $fm->{'path'} =~ m/^\// ) { $fm->{'path'} = $dir . $fm->{'path'}; }
+	my $s =
 "hisat2 -x $genome --sra-acc $fm->{'total'} --threads $options->{proc} --add-chrname > $p/$fm->{'filename_core'}_hisat.sam\n";
-    return $s, "$p/$fm->{'filename_core'}_hisat.sam", "$p/$fm->{'filename_core'}_hisat.sorted.bam";
+	return $s, "$p/$fm->{'filename_core'}_hisat.sam",
+	  "$p/$fm->{'filename_core'}_hisat.sorted.bam";
 }
 
 sub create_paired_call {
-    my $file = shift(@files);
-    my $pair = shift(@files);
-    my $fm   = root->filemap($file);
-    my $fm2  = root->filemap($pair);
-	my $p = $outpath;
+	my $file = shift(@files);
+	my $pair = shift(@files);
+	my $fm   = root->filemap($file);
+	my $fm2  = root->filemap($pair);
+	my $p    = $outpath;
 	$p ||= "$fm->{'path'}/HISAT2_OUT/";
-    $fm->{'path'} = "" if ( $fm->{'path'} eq "./" );
-    $fm2->{'path'} = "" if ( $fm2->{'path'} eq "./" );
-    $fm->{'path'} .= '/' unless ( $fm->{'path'} =~ m/\/$/ );
-    $fm2->{'path'} .= '/' unless ( $fm2->{'path'} =~ m/\/$/ );
-    mkdir ( "$p/" ) unless ( -d "$p/");
-    unless ( $fm->{'path'} =~ m/^\// ) { $fm->{'path'} = $dir . $fm->{'path'}; }
-    my $s =
+	$fm->{'path'}  = "" if ( $fm->{'path'} eq "./" );
+	$fm2->{'path'} = "" if ( $fm2->{'path'} eq "./" );
+	$fm->{'path'}  .= '/' unless ( $fm->{'path'} =~ m/\/$/ );
+	$fm2->{'path'} .= '/' unless ( $fm2->{'path'} =~ m/\/$/ );
+	mkdir("$p/") unless ( -d "$p/" );
+	unless ( $fm->{'path'} =~ m/^\// ) { $fm->{'path'} = $dir . $fm->{'path'}; }
+	my $s =
 "hisat2 -x $genome -1 $fm->{'total'} -2 $fm2->{'total'} --threads $options->{proc} --add-chrname > $p/$fm->{'filename_core'}_hisat.sam\n";
-    return $s, "$p/$fm->{'filename_core'}_hisat.sam", "$p/$fm->{'filename_core'}_hisat.sorted.bam";
+	return $s, "$p/$fm->{'filename_core'}_hisat.sam",
+	  "$p/$fm->{'filename_core'}_hisat.sorted.bam";
 }
 
 sub chk_cmd {
